4e29d408e3d1e1379b21e402ac276c6d2589b66e
kimzehong <nevergive5p@naver.com>
2024-09-05 16:20:52 +0900

    react 0905

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ import reportWebVitals from './reportWebVitals';
 import ConfirmDialog from "./ch04/ConfirmDialog";
 import Button from "./ch04/Button.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <ConfirmDialog />
-  </React.StrictMode>
-);
+import Library from './ch04/Clock';
+import Clock from './ch04/Clock';
 
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval(() => {
+
+
+    root.render(
+        <React.StrictMode>
+            <Clock/>
+        </React.StrictMode>
+    );
+},1000);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
