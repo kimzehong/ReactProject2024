8abf384111acd05adc2a9129ad5db1e04b00d909
kimzehong <nevergive5p@naver.com>
2024-09-12 16:02:06 +0900

    react 0912 + 5장

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './ch03/Book.css';
-import App from './App';
+// import './index.css';
+// import './ch03/Book.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ConfirmDialog from "./ch04/ConfirmDialog";
 import Button from "./ch04/Button.css";
-
+import Clock from "./ch04/Clock"
 import Library from './ch04/Clock';
-import Clock from './ch04/Clock';
+import CommentList from "./ch05/CommentList";
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() => {
 
-
-    root.render(
-        <React.StrictMode>
-            <Clock/>
-        </React.StrictMode>
+root.render(
+    <React.StrictMode>
+        <CommentList />
+    </React.StrictMode>
     );
-},1000);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
