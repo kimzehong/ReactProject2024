4d27293a1ac6027fe5e365a78a79ef88b9263e08
kimzehong <nevergive5p@naver.com>
2024-09-19 16:24:09 +0900

    react 0919 + 6장

---
+++ src/ch06/Notification.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Japan.css"
+
+// const styles ={
+//     wrapper: {
+//         margin: 8,
+//         padding: 8,
+//         display: "flex",
+//         flexDirection: "row",
+//         border: "1px solid grey",
+//         borderRadius: 16,
+//
+//     },
+//     messageText: {
+//         color: "magenta",
+//         fontSize: 16,
+//     },
+// };
+
+
+class Notification extends React.Component{
+
+    constructor(props) {
+        super(props); //Component가 생성될 때 prop를 전달받을 수 있는 매개변수를 갖는 생성가자 호출된다.
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return(
+            <div className="wrapper">
+                <span className="messageText">
+                    {this.props.message}
+                </span>
+            </div>
+        )
+    }
+}
+export default Notification;
